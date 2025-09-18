c3cb2e960006da184f29de7a1a4903cdee4d8e0b
Manuakiaa <m_anuarbek@kbtu.kz>
2025-09-18 12:01:03 +0500

    test2

---
+++ nvm.py
@@ -0,0 +1,19 @@
+A=int(input())
+B=[]
+B = list(map(int, input().split()))
+C=[]
+for i in range(1,A+1):
+    C.append(-1)
+    
+for i in range(1,A):
+    for j in range(0,i+1):
+        if B[i] > B[j]:
+            C[i] = B[j]
+            
+for i in range(0, A):
+    print(C[i], " ")
+
+
+#5
+# 2 1 5 8 3
+#-1 -1 1 5 1 
